b62cbbc15bbc89f947ca05ef20d65666244f1237
neandermenezes <neander2801@gmail.com>
2022-02-24 15:14:41 -0300

    req 3

--- middlewares/productsValidation.js
+++ middlewares/productsValidation.js
@@ -15,12 +15,12 @@ const nameValidation = async (req, res, next) => {
 
 const quantityValidation = async (req, res, next) => {
   const { quantity } = req.body;
-  if (!quantity || typeof quantity !== 'number') {
-    return res.status(400).json({ message: '"quantity" is required' });
-  }
-
   if (quantity <= 0) {
     return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
+  }
+
+  if (!quantity || typeof quantity !== 'number') {
+    return res.status(400).json({ message: '"quantity" is required' });
   }
 
   next();
--- middlewares/salesValidation.js
+++ middlewares/salesValidation.js
@@ -1,26 +1,23 @@
 const idValidation = async (req, res, next) => {
   const idData = req.body;
   const isInvalid = idData.some((obj) => obj.productId === undefined);
-  if (isInvalid) { return res.status(400).json({ message: '"productId" is required' }); }
+  if (isInvalid) {
+    return res.status(400).json({ message: '"productId" is required' });
+  }
 
   next();
 };
 
 const salesQuantityValidation = async (req, res, next) => {
   const quantityData = req.body;
+  console.log(quantityData);
+  const isMissingQuantity = quantityData.some((obj) => obj.quantity === undefined);
+  if (isMissingQuantity) { return res.status(400).json({ message: '"quantity" is required' }); }
 
-  const quantityNotInformed = quantityData.some(
-    ({ quantity }) => quantity !== undefined && typeof quantity !== 'number',
-  );
-
-  const quantityInvalid = quantityData.some(({ quantity }) => quantity <= 0);
-
-  if (quantityNotInformed) { return res.status(400).json({ message: '"quantity" is required' }); }
-  if (quantityInvalid) {
- return res
-      .status(422)
-      .json({ message: '"quantity" must be greater than or equal to 1' });
-}
+  const invalidQuantity = quantityData.some(({ quantity }) => quantity <= 0);
+  if (invalidQuantity) {
+    return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
+  }
 
   next();
 };
