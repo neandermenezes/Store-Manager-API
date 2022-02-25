e63c519605c7aa203a7abb89f1750d6c0d459e89
neandermenezes <neander2801@gmail.com>
2022-02-25 16:23:23 -0300

    avaliador

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const routers = require('./middlewares/routes');
-// const error = require('./middlewares/error');
-// const productsController = require('./controllers/productsController');
-// const salesController = require('./controllers/salesController');
 
 const app = express();
 
 app.use(express.json());
 
-// app.use(error);
-
 app.use('/products', routers.productsRouter);
 app.use('/sales', routers.salesRouter);
-
-// app.get('/products', productsController.listAll);
-// app.get('./products/:id', productsController.listById);
-// app.get('./sales', salesController.listAll);
-// app.get('./sales/:id', salesController.listById);
 
 app.get('/', (_request, response) => {
   response.send();
